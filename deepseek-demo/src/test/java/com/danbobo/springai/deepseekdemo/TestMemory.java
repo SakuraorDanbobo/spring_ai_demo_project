7f4f40d46d98e41228d052c99f214842e6384720
Danbobo <ermuren@qq.com>
2025-08-22 14:16:37 +0800

    【功能新增】: 简单记忆对话，多轮问询(手动 or 拦截器实现)。

--- deepseek-demo/src/test/java/com/danbobo/springai/deepseekdemo/TestMemory.java
+++ deepseek-demo/src/test/java/com/danbobo/springai/deepseekdemo/TestMemory.java
@@ -1,18 +1,20 @@
 package com.danbobo.springai.deepseekdemo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.deepseek.DeepSeekChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.UUID;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author Danbobo
@@ -22,6 +24,18 @@ import java.util.UUID;
 @SpringBootTest
 @Slf4j
 public class TestMemory {
+
+    private ChatClient chatClient;
+
+    // 做初始化内容
+    @BeforeEach
+    public void init(@Autowired ChatClient.Builder builder,
+                     @Autowired ChatMemory chatMemory) {
+        chatClient = builder
+                .defaultAdvisors(PromptChatMemoryAdvisor.builder(chatMemory)
+                                                        .build())
+                .build();
+    }
 
     /**
      * 通过多轮对话实现记忆功能，  demo01 - 手动存储
@@ -93,4 +107,48 @@ public class TestMemory {
                       .getText());
     }
 
+    /**
+     * 通过多轮对话实现记忆功能  demo03 —— 多用户隔离
+     *
+     * @param deepSeekChatModel
+     */
+    @Test
+    public void testMemory03(@Autowired DeepSeekChatModel deepSeekChatModel,
+                             @Autowired ChatMemory chatMemory) {
+
+        // 用户1：第一次对话
+        String resp1 = chatClient.prompt()
+                                       .user("我是牧濑红莉栖，一名天才科学家少女。") // 单次用户提问，不包含任何历史记录
+                                       .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, "user01")) // 分组id设置，内容区分
+                                       .call()
+                                       .content();
+        log.info(resp1);
+        // 用户1：第二次对话
+        String resp2 = chatClient.prompt()
+                                       .user("我叫什么？")
+                                       .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, "user01"))
+                                       .call()
+                                       .content();
+        log.info(resp2);
+
+        // 用户2：第一次对话
+        String resp3 = chatClient.prompt()
+                                       .user("我叫什么？")
+                                       .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, "user02"))
+                                       .call()
+                                       .content();
+        log.info(resp3);
+    }
+
+
+    @TestConfiguration
+    public static class ChatMemoryConfig {
+        @Bean
+        ChatMemory chatMemory(ChatMemoryRepository chatMemoryRepository) {
+            return MessageWindowChatMemory.builder()
+                                          .maxMessages(10)  // 记录最新条数,栈先进先出方式更新内容
+                                          .chatMemoryRepository(chatMemoryRepository)
+                                          .build();
+        }
+    }
 }
